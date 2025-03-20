af089eb107d207d1948a3bbc37cfade75ce7c26b
Jess Nelson <jemane.19@gmail.com>
2025-03-19 22:46:12 -0600

    W02pt1

---
+++ nextjs-dashboard/app/lib/actions.ts
@@ -0,0 +1,10 @@
+'use server';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+
+const sql = postgres(process.env.POSTGRES_U
